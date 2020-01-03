78f96a41894a14fba1c40de49c9fb0e64f47643e
李杭杭 <lihh@cheche365.com>
2020-01-03 10:14:19 +0800

    [Summary]:修改判断字符串 '[(())]' 是否合理方法，添加测试用例[Reviewer]:刘果

--- src/main/java/linkedlist/StackSolution.java
+++ src/main/java/linkedlist/StackSolution.java
@@ -14,21 +14,22 @@ public class StackSolution {
      * @param s
      * @return
      */
-    public boolean isVaild(String s) {
+    public static boolean isVaild(String s) {
         Stack<Character> stack = new Stack<>();
         Map<Character, Character> map = new LinkedHashMap<>();
         map.put(')', '(');
         map.put(']', '[');
         map.put('}', '{');
         for (char c : s.toCharArray()) {
-            if (map.containsKey(c)) {
-                stack.push(c);
+            if (!map.containsKey(c)) {//不是右括号的时候
+                stack.add(c);
             } else if (stack.isEmpty() || !map.get(c).equals(stack.pop())) {
                 return false;
             }
         }
         return true;
     }
+
     /**
      * 判断字符串 '[(())]' 是否合理
      * 时间复杂度O(n^2) 空间复杂度O(n)
--- src/test/java/hhl/linkedlist/StackSolutionTest.java
+++ src/test/java/hhl/linkedlist/StackSolutionTest.java
@@ -1,19 +1,21 @@
 package hhl.linkedlist;
 
+import linkedlist.StackSolution;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
- *  Stack
+ * Stack
  */
 public class StackSolutionTest {
 
     @Test
-    public void demo() {
-        assertEquals("1", "1");
-
-
+    public void test_isVaild() {
+        String s = "[(())]";
+        assertTrue(StackSolution.isVaild(s));
+        s += "}";
+        assertFalse(StackSolution.isVaild(s));
     }
 
 }
