cd1156b96ce78bfea1f57231f972b5356c49f7e1
hanghangli1993 <hanghangli1993@gmail.com>
2020-01-03 08:57:39 +0800

    [Summary]:添加整体测试类（缺少node.js触发的husky）[Reviewer]:刘果

---
+++ src/test/java/hhl/linkedlist/MainTest.java
@@ -0,0 +1,11 @@
+package hhl.linkedlist;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LikedListSolutionTest.class,
+        StackSolutionTest.class
+})
+public class MainTest {
+}
---
+++ src/test/java/hhl/linkedlist/StackSolutionTest.java
@@ -0,0 +1,19 @@
+package hhl.linkedlist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *  Stack
+ */
+public class StackSolutionTest {
+
+    @Test
+    public void demo() {
+        assertEquals("1", "1");
+
+
+    }
+
+}
