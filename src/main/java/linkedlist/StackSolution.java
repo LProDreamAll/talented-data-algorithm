c4c7e7c67d0405c197ff166270ae2cb55bfdf69d
LIU GUO <liuguo@asiainfo.com>
2020-01-03 13:24:44 +0800

    新增字符串匹配方法

--- src/main/java/linkedlist/StackSolution.java
+++ src/main/java/linkedlist/StackSolution.java
@@ -2,6 +2,8 @@ package linkedlist;
 
 import java.util.*;
 
+
+
 /**
  * Stack
  */
@@ -29,6 +31,7 @@ public class StackSolution {
         }
         return true;
     }
+
     /**
      * 判断字符串 '[(())]' 是否合理
      * 时间复杂度O(n^2) 空间复杂度O(n)
@@ -43,5 +46,32 @@ public class StackSolution {
             s = s.replace("()", "").replace("[]", "").replace("{}", "");
         } while (len != s.length());
         return s.length() == 0;
+    }
+
+    /**
+     * 判断字符串 '[(())]' 是否合理
+     * 时间复杂度O(n) 空间复杂度O(1)
+     * 找到中间点，从中间往两边扩散匹配
+     * @param s
+     * @return
+     */
+    public boolean isValid2(String s) {
+        Map<Character, Character> map = new LinkedHashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        if (Objects.nonNull(s) && s.length() % 2 > 0) {
+            return false;
+        }
+        int middle = s.length() / 2;
+        for (int i = middle - 1, j = middle; i >= 0;
+             i--, j++) {
+            Character close = s.charAt(j);
+            Character begin = s.charAt(i);
+            if (!close.equals(map.get(begin))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
