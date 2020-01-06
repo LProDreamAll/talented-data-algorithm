e756cce43384a3d3d230f4ada854d5dd96e6cdaa
hanghangli1993 <hanghangli1993@gmail.com>
2020-01-06 22:26:27 +0800

    [Summary]:1、添加显式的测试方法[Reviewer]:lhh

--- src/test/java/hhl/linkedlist/TestStackSolution.java
+++ src/test/java/hhl/linkedlist/TestStackSolution.java
@@ -1,11 +1,10 @@
 package hhl.linkedlist;
 
 import linkedlist.StackSolution;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
@@ -16,12 +15,13 @@ class TestStackSolution {
 
 
     @Test
-    @DisplayName("判断字符串 '[(())]' 是否合理")
+    @DisplayName("改用显式的方式判断字符串 '[(())]' 是否合理")
     void test_isVaild() {
-        String s = "[(())]";
-        assertTrue(StackSolution.isVaild(s));
-        s += "}";
-        assertFalse(StackSolution.isVaild(s));
+        String[] strings ={"[(())]","[(())]}"};
+        Assertions.assertAll("strings",
+                () -> Assertions.assertTrue(StackSolution.isVaild(strings[0])),
+                () -> Assertions.assertFalse(StackSolution.isVaild(strings[1]))
+        );
     }
 
 }
